2e39984301cc8c15a3e7a06a54bd9cdb4d02a63d
yakovlevav <yakovlevav@txix.ru>
2024-05-04 08:52:02 +0300

    init commit

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,19 +1,33 @@
 import arrow from 'src/images/arrow.svg';
+import { SyntheticEvent, useEffect, useState } from 'react';
+import { clsx } from 'clsx';
 
 import styles from './ArrowButton.module.scss';
 
 /** Функция для обработки открытия/закрытия формы */
-export type OnClick = () => void;
+export type OnClick = (state: boolean) => void;
 
-export const ArrowButton = () => {
+export const ArrowButton = ({onClick, isOpenForm} :{onClick: OnClick; isOpenForm?: boolean}) =>{
+	const arrowClickHandler = () => {
+		onClick?.(!isOpenForm)
+	}
+
+	useEffect(() => {
+		console.log("RENDER")
+	}, []);
+
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={clsx(styles.container, {[styles.container_open]: isOpenForm})}
+			onClick = {(e: SyntheticEvent) => {
+				e.stopPropagation()
+				arrowClickHandler()
+			}}>
+			<img src={arrow} alt='иконка стрелочки' className={clsx(styles.arrow, {[styles.arrow_open]: isOpenForm})} />
 		</div>
 	);
 };
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,133 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
+import clsx from 'clsx';
+import {
+	fontColors,
+	fontFamilyOptions,
+	backgroundColors,
+	contentWidthArr,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
+import { Select } from '../select';
 
 import styles from './ArticleParamsForm.module.scss';
+import { SyntheticEvent, useRef, useState } from 'react';
+import { useOutsideClickClose } from '../select/hooks/useOutsideClickClose';
+import { ArticleStateType } from 'src/constants/articleProps';
+import { Separator } from '../separator';
+import { RadioGroup } from '../radio-group';
+import { Text } from '../text';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	currentArticleState: ArticleStateType;
+	setCurrentArticleState: (params: any) => void;
+};
+
+export const ArticleParamsForm = ({
+	currentArticleState,
+	setCurrentArticleState,
+}: ArticleParamsFormProps) => {
+	const rootRef = useRef<HTMLDivElement>(null);
+	const [isOpenForm, setIsOpenForm] = useState<boolean>(false);
+	const [newFontColor, setNewFontColor] = useState(
+		currentArticleState.fontColor
+	);
+	const [newFontFamily, setNewFontFamily] = useState(
+		currentArticleState.fontFamilyOption
+	);
+	const [newBackgroundColor, setNewBackgroundColor] = useState(
+		currentArticleState.backgroundColor
+	);
+	const [newContentWidth, setNewContentWidth] = useState(
+		currentArticleState.contentWidth
+	);
+	const [newFontSize, setNewFontSize] = useState(
+		currentArticleState.fontSizeOption
+	);
+
+	useOutsideClickClose({
+		isOpen: isOpenForm,
+		rootRef,
+		onClose: () => setIsOpenForm(false),
+		onChange: setIsOpenForm,
+	});
+
+	const formSubmitHandler = (e: SyntheticEvent) => {
+		e.preventDefault();
+
+		setCurrentArticleState({
+			...currentArticleState,
+			fontColor: newFontColor,
+			fontFamilyOption: newFontFamily,
+			backgroundColor: newBackgroundColor,
+			contentWidth: newContentWidth,
+			fontSizeOption: newFontSize,
+		});
+	};
+
+	const formResetHandler = (e: SyntheticEvent) => {
+		e.preventDefault();
+		setNewFontColor(fontColors[0]);
+		setNewFontSize(fontSizeOptions[0]);
+		setNewFontFamily(fontFamilyOptions[0]);
+		setNewBackgroundColor(backgroundColors[0]);
+		setNewContentWidth(contentWidthArr[0]);
+		setCurrentArticleState({
+			...currentArticleState,
+			fontColor: fontColors[0],
+			fontFamilyOption: fontFamilyOptions[0],
+			backgroundColor: backgroundColors[0],
+			contentWidth: contentWidthArr[0],
+			fontSizeOption: fontSizeOptions[0],
+		});
+	};
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton onClick={setIsOpenForm} isOpenForm={isOpenForm} />
+			<aside
+				ref={rootRef}
+				className={clsx(styles.container, isOpenForm && styles.container_open)}>
+				<form
+					className={styles.form}
+					onSubmit={formSubmitHandler}
+					onReset={formResetHandler}
+					style={{ flexDirection: 'column', gap: 50 }}>
+					<Text as='h2' size={31} weight={800} uppercase family='open-sans'>
+						Задайте параметры
+					</Text>
+					<Select
+						options={fontFamilyOptions}
+						selected={newFontFamily}
+						title='шрифт'
+						onChange={setNewFontFamily}
+					/>
+					<RadioGroup
+						options={fontSizeOptions}
+						selected={newFontSize}
+						title='размер шрифта'
+						name='размер шрифта'
+						onChange={setNewFontSize}
+					/>
+					<Select
+						options={fontColors}
+						selected={newFontColor}
+						title='цвет шрифта'
+						onChange={setNewFontColor}
+					/>
+					<Separator />
+					<Select
+						options={backgroundColors}
+						selected={newBackgroundColor}
+						title='цвет фона'
+						onChange={setNewBackgroundColor}
+					/>
+					<Select
+						options={contentWidthArr}
+						selected={newContentWidth}
+						title='ширина контента'
+						onChange={setNewContentWidth}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
